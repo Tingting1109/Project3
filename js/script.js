9703151717ea7bebfcac31256f8c8ba9b1ad7538
Tingting1109 <89550675+Tingting1109@users.noreply.github.com>
2022-04-11 23:54:11 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -10,4 +10,6 @@ function init()
   entrybtn.addEventListener('click',clickAlertMe);
   
 }
-window.addEventListener('load', init);
+window.addEventListener('load', init);'
+
+<script type="text/javascript' src="https://www.google.com/maps?file=api&hl=zh-CN&v=2&key=abcdefg" type="text/javascript"></script>
