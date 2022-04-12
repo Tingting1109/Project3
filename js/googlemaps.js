53eeb65f77737686456d81b116302891f7e5741c
Tingting1109 <89550675+Tingting1109@users.noreply.github.com>
2022-04-12 13:30:33 -0500

    Update googlemaps.js

--- js/googlemaps.js
+++ js/googlemaps.js
@@ -1,1 +1,7 @@
+<script src="https://www.google.com/maps?file=api&hl=zh-CN&v=2&key=abcdefg" type="js/script.js"></script>
+  var map = new Map2(document.getElementByIdx_x_x("map"),
+   {size:GSize(200,200),backgroundColor:"#FF0000"});
 
+   map.addControl(new GSmallMapControl());
+   map.addControl(new GMapTypeControl());
+   map.setCenter(new GLatLng(37.4419,-122.1419), 13);
