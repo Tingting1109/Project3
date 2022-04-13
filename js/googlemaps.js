8e83f452c0cbfb6da29b84a11e99ab05ffcd55ab
Tingting1109 <89550675+Tingting1109@users.noreply.github.com>
2022-04-12 22:01:05 -0500

    Delete googlemaps.js

--- js/googlemaps.js
+++
@@ -1,7 +0,0 @@
-
-  var map = new Map2(document.getElementByIdx_x_x("map"),
-   {size:GSize(200,200),backgroundColor:"#FF0000"});
-
-   map.addControl(new GSmallMapControl());
-   map.addControl(new GMapTypeControl());
-   map.setCenter(new GLatLng(37.4419,-122.1419), 13);
