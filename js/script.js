ada2f248edf0f4736a8c56880c10bc3d995b1c82
Tingting1109 <89550675+Tingting1109@users.noreply.github.com>
2022-04-12 23:27:57 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
 function initMap() {
     var map;
-    var csLatLang = new google.maps.LatLng(41.83863002472576, -87.6270380886158);
+    var sbLatLang = new google.maps.LatLng(41.83873002472576, -87.6270380886158);
     var parkingLatLng = new google.maps.LatLng(41.838566078590155, -87.62616905289498)
     var parkingSpot2LatLang = new google.maps.LatLng(41.83747898404313, -87.62616905289498)
     var infoLatLang = new google.maps.LatLng(41.83855433094542, -87.6273903853663);
-    var scienceLatLang = new google.maps.LatLng(41.83792661341855, -87.62704881745186)
+    var scienceLatLang = new google.maps.LatLng(41.83712661341855, -87.62704881745186)
     const contentString =
-        '<h1>CS Department</h1>' +
-        'This is where I go to most days' +
-        'This is the CS department. I have most of my CS classes here';
+        '<h1>Stuart Building</h1>' +
+        'Most of my major classes are in this building' +
+        'Most of the computer school classrooms are here';
     const scienceString =
-        '<h1>Science Department</h1>' +
-        'This is where I sometimes go. ' +
-        'Sometimes I have CS courses in this building';
+        '<h1>John T. Rettaliata Engineering Center</h1>' +
+        'My language classes are in this building ' +
+        'One of my specialized courses was also here last semester';
     const infowindow = new google.maps.InfoWindow({
         content: contentString,
     });
